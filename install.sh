61f6c9596b2b30004dc934bea977a7d72ff28082
Nicole Yu <x1n90623@gmail.com>
2024-06-09 17:23:30 +1000

    first commit

---
+++ install.sh
@@ -0,0 +1,21 @@
+# fix me: popeye
+https://github.com/derailed/popeye/releases/download/v0.21.3/popeye_0.8.0_Linux_x86_64.tar.gz
+popeye_darwin_amd64
+
+tar -xzvf <>
+sudo mv popeye /usr/local/bin
+
+
+wget https://github.com/derailed/xray/releases/download/v0.3.2/xray_0.3.2_Linux_x86_64.tar.gz
+tar -xzvf xray_0.3.2_Linux_x86_64.tar.gz
+sudo mv xray /usr/local/bin
+
+
+
+https://github.com/derailed/k9s/blob/master/README.md
+
+
+Unix	macOS	Windows
+~/.config/k9s	~/Library/Application Support/k9s	%LOCALAPPDATA%\k9s
+
+
