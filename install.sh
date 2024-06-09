87b7d042545c2a811bf8520cf793b9d1b77c496d
Nicole Yu <x1n90623@gmail.com>
2024-06-09 17:36:57 +1000

    second commit

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 # fix me: popeye
-https://github.com/derailed/popeye/releases/download/v0.21.3/popeye_0.8.0_Linux_x86_64.tar.gz
+
+https://api.github.com/repos/derailed/popeye/releases/latest
+https://github.com/derailed/popeye/releases/download/v0.21.3/popeye_linux_amd64.tar.gz
 popeye_darwin_amd64
 
 tar -xzvf <>
@@ -19,3 +21,10 @@ Unix	macOS	Windows
 ~/.config/k9s	~/Library/Application Support/k9s	%LOCALAPPDATA%\k9s
 
 
+
+
+DIVE_VERSION=$(curl -sL "https://api.github.com/repos/wagoodman/dive/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
+curl -OL https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.rpm
+rpm -i dive_${DIVE_VERSION}_linux_amd64.rpm
+
+https://github.com/wagoodman/dive
